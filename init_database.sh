88da64c01a3f8757228445a0c985d0986fe28224
diamondburned <diamond@arikawa-hi.me>
2023-10-24 13:03:00 -0700

    Add authentication schema_init to init_database

--- init_database.sh
+++ init_database.sh
@@ -3,3 +3,6 @@ set -e
 
 echo "Initializing enrollment database..."
 python3 -m services.enrollment.schema_init
+
+echo "Initializing authentication database..."
+python3 -m services.authentication.schema_init
