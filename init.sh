23b73f789dcf08b68ec0df0bc573f8a36066e740
diamondburned <diamond@arikawa-hi.me>
2023-10-27 22:59:18 -0700

    Fix init.sh for locally-installed litefs

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if ! command -v litefs >/dev/null 2>&1; then
+if ! PATH="$PATH":run/bin command -v litefs >/dev/null 2>&1; then
 	echo "litefs isn't even installed, did you run ./install_deps.sh?" >&2
 	exit 1
 fi
