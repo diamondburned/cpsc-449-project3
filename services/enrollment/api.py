c5a2e563efa138000bce4d2f97923cc2e0341deb
diamondburned <diamond@arikawa-hi.me>
2023-11-03 01:15:39 -0700

    Add missing endpoints into krakend

--- services/enrollment/api.py
+++ services/enrollment/api.py
@@ -267,7 +267,12 @@ def list_user_sections(
 def list_user_waitlist(
     user_id: int,
     db: sqlite3.Connection = Depends(get_db),
+    jwt_user: int = Depends(require_x_user),
+    jwt_roles: list[Role] = Depends(require_x_roles),
 ) -> ListUserWaitlistResponse:
+    if Role.REGISTRAR not in jwt_roles and jwt_user != user_id:
+        raise HTTPException(status_code=403, detail="Not authorized")
+
     section_ids = fetch_rows(
         db,
         """
